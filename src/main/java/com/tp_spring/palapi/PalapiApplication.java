5a2afee6e1191fa95b06485f24f2a51f5a468d17
Samrou Teouri <teourisamrou@gmail.com>
2024-11-20 17:48:06 +0100

    SpringBoot Project Initialization

---
+++ src/main/java/com/tp_spring/palapi/PalapiApplication.java
@@ -0,0 +1,13 @@
+package com.tp_spring.palapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp_spring/palapi/PalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp_spring.palapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
