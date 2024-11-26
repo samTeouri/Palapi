f42e06ea413ee99979b6b82b12abbeb7764b9604
Samrou Teouri <teourisamrou@gmail.com>
2024-11-26 02:01:18 +0100

    Constructor for Pal class

--- src/main/java/com/tp_spring/palapi/model/Pal.java
+++ src/main/java/com/tp_spring/palapi/model/Pal.java
@@ -6,13 +6,12 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakatra.persistence.Id;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Table(name="pals")
@@ -20,21 +19,18 @@ public class Pal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String key;
     
+    private String key;
     private String name;
-
     private String wiki;
-
-    @ElementCollection
+    private String rarity;
+    private double price;
+    private String size;
     private List<String> types = new ArrayList<>();
-
+    private List<String> drops = new ArrayList<>();
+    
     @ElementCollection
     private List<Suitability> suitability = new ArrayList<>();
-
-    @ElementCollection
-    private List<String> drops = new ArrayList<>();
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Skill> skills = new ArrayList<>();
@@ -42,11 +38,32 @@ public class Pal {
     @Embedded
     private Stats stats;
 
-    private String rarity;
-
-    private double price;
-
-    private String size;
+    // Constructor
+    public Pal(
+        String _key,
+        String _name,
+        String _wiki,
+        String _rarity,
+        double _price,
+        String _size,
+        List<String> _types,
+        List<String> _drops,
+        List<Suitability> _suitability,
+        List<Skill> _skills,
+        Stats _stats
+        ) {
+            this.key = _key;
+            this.name = _name;
+            this.wiki = _wiki;
+            this.rarity = _rarity;
+            this.price = _price;
+            this.size = _size;
+            this.types = _types;
+            this.drops = _drops;
+            this.suitability = _suitability;
+            this.skills = _skills;
+            this.stats = _stats;
+    }
 
 
 
--- src/main/java/com/tp_spring/palapi/model/Skill.java
+++ src/main/java/com/tp_spring/palapi/model/Skill.java
@@ -1,6 +1,15 @@
 package com.tp_spring.palapi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int level;
     private String name;
     private String type;
