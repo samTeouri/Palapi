56645a976c99afc2bbd646cef46d4cdb935bae0a
Samrou Teouri <teourisamrou@gmail.com>
2024-11-30 04:42:21 +0100

    Controller : Get Pal by Id

--- src/main/java/com/tp_spring/palapi/web/PalController.java
+++ src/main/java/com/tp_spring/palapi/web/PalController.java
@@ -1,8 +1,12 @@
 package com.tp_spring.palapi.web;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tp_spring.palapi.application.dto.PalDTO;
 import com.tp_spring.palapi.application.service.PalService;
 
 @RestController
@@ -14,5 +18,9 @@ public class PalController {
     public PalController(PalService palService) {
         this.palService = palService;
     }
-    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PalDTO> getPalById(@PathVariable Long id) {
+        return ResponseEntity.ok(palService.getPalById(id));
+    }
 }
