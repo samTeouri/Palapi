adb4a7efa3e1a6fdd94648474addb3db39258429
Samrou Teouri <teourisamrou@gmail.com>
2024-11-30 04:43:42 +0100

    Controller : Create a Pal

--- src/main/java/com/tp_spring/palapi/web/PalController.java
+++ src/main/java/com/tp_spring/palapi/web/PalController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,10 @@ public class PalController {
     @GetMapping("/{id}")
     public ResponseEntity<PalDTO> getPalById(@PathVariable Long id) {
         return ResponseEntity.ok(palService.getPalById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<PalDTO> createPal(@RequestBody PalDTO palDTO) {
+        return ResponseEntity.ok(palService.createPal(palDTO));
     }
 }
